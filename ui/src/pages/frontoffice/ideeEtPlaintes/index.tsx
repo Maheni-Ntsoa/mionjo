12689ec7ca0143a84c8b7bec3dd7791d4f976a21
Mahenintsoa <razevasonmahenintsoa@gmail.com>
2023-11-15 11:04:11 +0300

    Update index.tsx

--- ui/src/pages/frontoffice/ideeEtPlaintes/index.tsx
+++ ui/src/pages/frontoffice/ideeEtPlaintes/index.tsx
@@ -1,6 +1,5 @@
 import IdeePourLeSud from "./components/IdeePourLeSud";
 import Plaintes from "./components/Plaintes";
-import StatGestionPlainte from "./components/StatGestionPlainte";
 
 const text = `Ecrivez-nous votre idée`;
 const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum libero ut volutpat elementum. Nullam non sem tristique, accumsan nibh eu, maximus ligula. In posuere, lacus eu pulvinar cursus, erat ante vulputate erat, a volutpat dui urna laoreet metus. Curabitur egestas justo at nibh imperdiet malesuada. Sed vestibulum sem eget orci maximus semper. Phasellus tellus tellus, porta sit amet lectus efficitur, egestas sodales risus. Curabitur iaculis convallis interdum. Quisque cursus posuere sapien rutrum venenatis. Phasellus et est et magna semper placerat. Nulla vel neque vel sem fermentum fermentum vel nec nulla. Donec pretium metus augue, vel interdum nisl iaculis at.`;
@@ -15,9 +14,9 @@ const IdeeEtPlaintes = () => {
       <div className="flex justify-center my-12" id="deposer">
         <Plaintes />
       </div>
-      <div className="flex justify-center " id="stats">
+      {/* <div className="flex justify-center " id="stats">
         <StatGestionPlainte />
-      </div>
+      </div> */}
     </div>
   );
 };
