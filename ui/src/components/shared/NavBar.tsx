6eb3fa4a22e8335f3030fbf93fd9cbce94d270bf
Fanomezantsoa0105 <fanomezantsoa.ranaivoarilala@mionjo.mg>
2025-07-03 12:03:13 +0300

    Enlever lien Cop29

--- ui/src/components/shared/NavBar.tsx
+++ ui/src/components/shared/NavBar.tsx
@@ -568,7 +568,7 @@ const Navbar = () => {
                 </Link>
               </li>
 
-              <li className="group relative text-white px-2 py-2 rounded hover:bg-black/10">
+              {/* <li className="group relative text-white px-2 py-2 rounded hover:bg-black/10">
                 <Link to="https://cop29.mapmionjo.mg" target="_blank">
                   <span
                     className="font-quicksand text-md max-w-fit uppercase"
@@ -577,7 +577,7 @@ const Navbar = () => {
                     {t("cop29")}
                   </span>
                 </Link>
-              </li>
+              </li> */}
             </ul>
           </div>
         </div>
